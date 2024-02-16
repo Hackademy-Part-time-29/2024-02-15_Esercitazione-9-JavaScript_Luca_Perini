c589fbac4d3897371fd3d188f6f20dece2d6b5d8
zeros <luca.perini.illustrations@gmail.com>
2024-02-16 18:41:08 +0100

    esercizio javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+let num1 = 10;
+
+let num2 = 20;
+
+console.log("il valore della variabile num1 è:");
+
+console.log(num1);
+
+console.log("il valore della variabile num2 è:");
+
+console.log(num2);
+
+let stringa = 300;
+
+console.log("il valore della stringa è:");
+
+console.log(stringa);
+
+stringa = 450;
+
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+
+console.log("Il valore della costante è:");
+
+console.log(PIGRECO);
+
+PIGRECO = 314;
